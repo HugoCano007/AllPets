9c403a3f7bc3c83723250f8c03da173640ecd74e
HugoCano007 <hugocanolopez309@gmail.com>
2026-03-26 16:26:05 -0500

    app.js server.js .env

---
+++ backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middlewares globales
+app.use(cors());
+app.use(express.json());
+
+// Ruta base
+app.get("/api/v1", (req, res) => {
+    res.json({
+        message: "VetControl API funcionando correctamente 🐾"
+    });
+});
+
+module.exports = app;
